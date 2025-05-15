a8f074b8a2bf5ae185d22b2d705d8b6e66f1ce93
Chemseddine <chamseddinexd@gmail.com>
2025-05-15 21:25:16 +0200

    figma

--- app/ProductDetails/[id].tsx
+++ app/ProductDetails/[id].tsx
@@ -11,6 +11,7 @@ const ProductDetails = () => {
     const response = await fetch(`https://fakestoreapi.com/products/${id}`);
     const data = await response.json();
     setProduct(data);
+
   }
 
   useEffect(() => {
